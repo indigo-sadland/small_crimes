9c25c23e459d899ee97d95cea7332f2ce6c62e62
indigo <mr.wild17@gmail.com>
2022-02-07 17:50:48 +0300

    ffuf, httpx examples

--- small_crimes.sh
+++ small_crimes.sh
@@ -12,10 +12,11 @@ if [[ -z $1 ]] || [[ $1 == "help" ]]; then
     echo '
 Usage:
     words - keyword for showing wordlists. 
-                Available: content, api, params, dns, fuzz, xss, sqli, lfi
+                Available flags: content, api, params, dns, fuzz, xss, sqli, lfi
 
     ffuf  - keyword for showing ffuf command exmaples. 
-                Availabe: discovery, vhost, paramMining
+
+    https - keyword for showing httpx command exmaples.
 
     help - show this message.        
 '
@@ -61,6 +62,10 @@ seclists_lfi="$home_recon/wrds/SecLists/Fuzzing/LFI/"
 
 # wordlists keyword
 if [[ $1 == "words" ]]; then
+
+    if [[  -z $2 ]]; then
+        echo -e '\033[1mFlag is required!\033[0m'
+    fi
 
     if [[ $2 == "content" ]]; then
 
@@ -238,22 +243,35 @@ fi
 # ffuf keyword
 if [[ $1 == "ffuf" ]]; then
 
-    if [[ $2 == "discovery" ]]; then
-        echo -e '\033[1mFiles and Dirs Discovery Example Command\033[0m'
-        echo "ffuf -w ${seclists_web_content} -e .php,.html,.sql,.bak,.old,.txt,.zip -c -v -r -u http://ffuf.me/FUZZ"
 
-    fi
+    echo -e '\033[1mFiles and Dirs Discovery Example Command\033[0m'
+    echo "ffuf -w ${seclists_web_content} -e .php,.html,.sql,.bak,.old,.txt,.zip -c -v -r -u http://ffuf.me/FUZZ"
+    echo ""
 
+    echo -e '\033[1mVHost Discovery Example Command\033[0m'
+    echo "ffuf -w ${seclists_dns} -H "Host: FUZZ.ffuf.me" -u http://ffuf.me"
+    echo ""
 
-    if [[ $2 == "vhost" ]]; then
-        echo -e '\033[1mVHost Discovery Example Command\033[0m'
-        echo "ffuf -w ${seclists_dns} -H "Host: FUZZ.ffuf.me" -u http://ffuf.me"
+    echo -e '\033[1mFuzzing Multiple Locations\033[0m'
+    echo "ffuf -u https://W2/W1 -w ./wordlist.txt:W1,./domains.txt:W2"
+    echo ""
 
-    fi
+    echo -e '\033[1mGET Parameters Discovery Example Command \033[0m'
+    echo "ffuf -w ${seclists_params} -u http://ffuf.me/cd/param/data?FUZZ=1"    
+    echo ""
 
+    echo -e '\033[1mPOST Parameters Discovery Example Command \033[0m'
+    echo "ffuf -w /path/to/postdata.txt -X POST -d 'username=admin\&password=FUZZ' -u https://target/login.php"
+    echo ""
     
-    if [[ $2 == "paramMining" ]]; then
-        echo -e '\033[1mParameters Discovery Example Command \033[0m'
-        echo "ffuf -w ${seclists_params} -u http://ffuf.me/cd/param/data?FUZZ=1"    
-    
+fi
+
+
+# httpx keyword
+if [[ $1 == "httxp" ]]; then
+
+    echo -e '\033[1mhttpx Full Scan Examlpe Command \033[0m'
+    echo "httpx -l domains.txt -vhost -http2 -pipeline -title -content-length -status-code -follow-redirects -tls-probe -content-type -location -csp-probe -web-server -stats -ip -cname -cdn -ports 80,81,300,443,591,593,832,981,1010,1311,2082,2087,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5800,6543,7000,7396,7474,8000,8001,8008,8014,8042,8069,8080,8081,8088,8090,8091,8118,8123,8172,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9200,9443,9800,9981,12443,16080,18091,18092,20720,28017 -threads 300 -o httpx.out"
+    echo ""
+
 fi
